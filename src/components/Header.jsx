747450264609cc48349c713041ab33407b58ec42
jmksm96 <60604825+jmksm96@users.noreply.github.com>
2020-09-28 14:04:32 +0300

    First release

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const Header = () => {
+    return(
+        <header className ='header'>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1280px-Node.js_logo.svg.png" alt=""/>
+        </header>
+    )
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+const Profile = () => {
+    return(
+        <div className= 'content'>
+            <div>
+                <img src="https://www.andreasreiterer.at/wp-content/uploads/2017/11/react-logo-825x510.jpg"/>
+            </div>
+            <div> ava + opisanie</div>
+            <div> my post
+                <div>
+                    You post
+                </div>
+            </div>
+            <div> post 1</div>
+            <div>post 2</div>
+        </div>
+    )
+}
+export default Profile;
