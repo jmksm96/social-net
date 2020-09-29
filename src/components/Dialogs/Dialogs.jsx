e88634e91133debf297240d85208949c82c68601
jmksm96 <60604825+jmksm96@users.noreply.github.com>
2020-09-29 21:07:51 +0300

    22. Добавил <NavLink> к диалогам.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import s from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
-    return  (
-        <div className={s.content}>
-            Dialogs
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={`${s.dialog} {s.active}`}>
+                    <NavLink to="/dialogs/1">Max</NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/2">Mark</NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/3">Danila</NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/4">Karina</NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/5">Dima</NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/6">Edward</NavLink></div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Your project?</div>
+                <div className={s.message}>Yeah</div>
+                <div className={s.message}></div>
+            </div>
         </div>
-        )
+    )
 }
 
 export default Dialogs;
