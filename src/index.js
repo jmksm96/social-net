82b4443b7f41677e9eb269b531a32b8baffc6d6e
jmksm96 <60604825+jmksm96@users.noreply.github.com>
2020-10-07 19:26:53 +0300

    37: OOP refactoring, add store in state.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import {renderEntireThree} from "./render";
-import state from "./redux/state";
+import store from "./redux/state";
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <React.StrictMode>
+                <App state={state} addPost = {store.addPost.bind(store)} updateNewPostText = {store.updateNewPostText.bind(store)}
+                     addMessage = {store.addMessage.bind(store)} updateNewMessageText = {store.updateNewMessageText.bind(store)}/>
+            </React.StrictMode>
+        </BrowserRouter>, document.getElementById('root')
+    );
+}
 
 
 
-renderEntireThree(state);
+store.subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+
 serviceWorker.unregister();
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,65 +1,74 @@
-import {renderEntireThree} from "../render";
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: "It's my first post", likesCount: '12'},
+                {id: 2, message: "Hello world", likesCount: '11'}
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: "Max"},
+                {id: 2, name: "Karina"},
+                {id: 3, name: "Mark"},
+                {id: 4, name: "Dimon"},
+                {id: 5, name: "Danila"},
+                {id: 6, name: "Edik"}
+            ],
+            messages: [
+                {id: 1, message: "Hi"},
+                {id: 2, message: "Your project?"},
+                {id: 3, message: "Yeah"},
+                {id: 4, message: "Cool, man"}
 
-let state = {
-    profilePage: {
-        posts: [
-            {id: 1, message: "It's my first post", likesCount: '12'},
-            {id: 2, message: "Hello world", likesCount: '11'}
-        ],
-        newPostText: ''
+            ],
+            newMessageText: ''
+        }
     },
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: "Max"},
-            {id: 2, name: "Karina"},
-            {id: 3, name: "Mark"},
-            {id: 4, name: "Dimon"},
-            {id: 5, name: "Danila"},
-            {id: 6, name: "Edik"}
-        ],
-        messages: [
-            {id: 1, message: "Hi"},
-            {id: 2, message: "Your project?"},
-            {id: 3, message: "Yeah"},
-            {id: 4, message: "Cool, man"}
+    getState() {
+        return this._state;
+    },
+    _callSubscriber() {
+        console.log('state changed')
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
 
-        ],
-        newMessageText: ''
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    addMessage() {
+        let newMessage = {
+            id: 5,
+            message: this._state.dialogsPage.newMessageText
+        }
+        this._state.dialogsPage.messages.push(newMessage);
+        this._state.dialogsPage.newMessageText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewMessageText(newMessage) {
+        this._state.dialogsPage.newMessageText = newMessage;
+        this._callSubscriber(this._state);
+    },
+    subscribe (observer) {
+        this._callSubscriber = observer
     }
+
 }
 
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    renderEntireThree(state);
-}
 
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    renderEntireThree(state);
-}
-
-export let addMessage = () => {
-    let newMessage = {
-        id:5,
-        message: state.dialogsPage.newMessageText
-    }
-    state.dialogsPage.messages.push(newMessage)
-    state.dialogsPage.newMessageText = '';
-    renderEntireThree(state);
-}
-
-export let updateNewMessageText = (newMessage) => {
-    state.dialogsPage.newMessageText = newMessage;
-    renderEntireThree(state);
-}
-
-export default state;
+export default store;
+window.store = store;
 
 
 
