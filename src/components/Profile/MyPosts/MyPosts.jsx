a56467eba6a5445371d7a0a1689ada6b1aad9f65
jmksm96 <60604825+jmksm96@users.noreply.github.com>
2020-10-09 14:22:49 +0300

    38-40: dispatch, action creator, dialogs fix.

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from './MyPosts.module.css'
 import Post from "./Posts/Post";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/state";
 
 
 const MyPosts = (props) => {
@@ -8,14 +9,13 @@ const MyPosts = (props) => {
         props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
 
     let newPostElem = React.createRef();
-
     let newPost = () => {
-        props.addPost();
+        props.dispatch(addPostActionCreator());
     }
-
     let onChange = () => {
         let text = newPostElem.current.value;
-        props.updateNewPostText(text);
+        let action = updateNewPostTextActionCreator(text);
+        props.dispatch(action);
     };
 
     return (
