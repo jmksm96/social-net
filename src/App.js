8c8ce77fd15f12efc365f91b55d74da5f6f734f9
jmksm96 <60604825+jmksm96@users.noreply.github.com>
2020-10-18 21:05:41 +0300

    68: redirect

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ import Music from "./components/Music/Music";
 import {BrowserRouter, Route} from "react-router-dom";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
-
-
-
+import Login from "./components/Login/Login";
 
 
 const App = (props) => {
@@ -24,13 +22,20 @@ const App = (props) => {
                 <div className='app-wrapper-content'>
                     <Route path='/dialogs'
                            render={() => <DialogsContainer/>}/>
+
                     <Route path='/profile/:userId?'
                            render={() => <ProfileContainer/>}/>
+
                     <Route path='/users'
-                           render={() => <UsersContainer />}/>
+                           render={() => <UsersContainer/>}/>
+
+                    <Route path='/login'
+                           render={() => <Login/>}/>
+
                     <Route path='/news' component={News}/>
                     <Route path='/settings' component={Settings}/>
                     <Route path='/music' component={Music}/>
+
                 </div>
             </div>
         </BrowserRouter>
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import s from "./Dialogs.module.css"
 import Message from "./Message/Message";
 import DialogItem from "./DialogItem/DialogItem";
+import {Redirect} from "react-router-dom";
 
 
 const Dialogs = (props) => {
@@ -21,6 +22,7 @@ const Dialogs = (props) => {
         props.updateNewMessageBody(body);
     }
 
+   if (!props.isAuth) return <Redirect to = {'/login'}/>
 
     return (
         <div className={s.dialogs}>
