602b3f8e4add9b3ad7da08eb672a057824c77f30
jmksm96 <60604825+jmksm96@users.noreply.github.com>
2020-10-20 19:06:29 +0300

    74: ComponentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -26,6 +26,16 @@ class ProfileStatus extends React.Component {
         });
     }
 
+    componentDidUpdate(prevProps, prevState ) {
+
+        if(prevProps.status !== this.props.status) {
+            this.setState({
+                status:this.props.status
+            })
+        }
+        console.log("componentDidUpdate")
+    }
+
     render() {
         return (
             <div>
