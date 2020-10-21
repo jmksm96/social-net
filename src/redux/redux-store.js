42f94d2fc203d709bb91633e7526a479a5dc1f3e
jmksm96 <60604825+jmksm96@users.noreply.github.com>
2020-10-21 14:16:39 +0300

    75:  LoginForm

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,6 +5,7 @@ import sideBarReducer from "./sidebar-reducer";
 import userReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
+import {reducer as formReducer} from 'redux-form'
 
 let reducers = combineReducers(
     {
@@ -12,7 +13,8 @@ let reducers = combineReducers(
         dialogsPage: dialogsReducer,
         sidebar: sideBarReducer,
         usersPage: userReducer,
-        auth: authReducer
+        auth: authReducer,
+        form: formReducer
     })
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware))
