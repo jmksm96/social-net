af96eef8119deb35700d52e907a9292b83f20add
jmksm96 <60604825+jmksm96@users.noreply.github.com>
2020-10-23 21:15:23 +0300

    79-80:  stopSubmit, Initialization App

--- src/App.js
+++ src/App.js
@@ -6,41 +6,62 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
 import Music from "./components/Music/Music";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, withRouter} from "react-router-dom";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import Login from "./components/Login/Login";
+import {connect} from "react-redux";
+import {compose} from "redux";
+import {initializeApp} from "./redux/app-reducer";
+import Preloader from "./components/common/Preloader/Preloader";
 
 
-const App = (props) => {
+class App extends React.Component {
 
-    return (
-        <BrowserRouter>
-            <div className='app-wrapper'>
-                <HeaderContainer/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Route path='/dialogs'
-                           render={() => <DialogsContainer/>}/>
+    componentDidMount() {
+        this.props.initializeApp();
+    }
 
-                    <Route path='/profile/:userId?'
-                           render={() => <ProfileContainer/>}/>
+    render() {
 
-                    <Route path='/users'
-                           render={() => <UsersContainer/>}/>
+        if (!this.props.initialized) {
+            return <Preloader/>
+        }
 
-                    <Route path='/login'
-                           render={() => <Login/>}/>
+        return (
+            <BrowserRouter>
+                <div className='app-wrapper'>
+                    <HeaderContainer/>
+                    <Navbar/>
+                    <div className='app-wrapper-content'>
+                        <Route path='/dialogs'
+                               render={() => <DialogsContainer/>}/>
 
-                    <Route path='/news' component={News}/>
-                    <Route path='/settings' component={Settings}/>
-                    <Route path='/music' component={Music}/>
+                        <Route path='/profile/:userId?'
+                               render={() => <ProfileContainer/>}/>
 
+                        <Route path='/users'
+                               render={() => <UsersContainer/>}/>
+
+                        <Route path='/login'
+                               render={() => <Login/>}/>
+
+                        <Route path='/news' component={News}/>
+                        <Route path='/settings' component={Settings}/>
+                        <Route path='/music' component={Music}/>
+
+                    </div>
                 </div>
-            </div>
-        </BrowserRouter>
-    )
+            </BrowserRouter>
+        )
+    }
 }
 
 
-export default App;
+const mapStateToProps = (state) => ({
+    initialized: state.app.initialized
+})
+
+export default compose(
+    withRouter,
+    connect(mapStateToProps, {initializeApp}))(App);
--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,4 +1,5 @@
 import {authAPI} from "../api/api";
+import {stopSubmit} from "redux-form";
 
 const SET_USER_DATA = 'SET_USER_DATA';
 
@@ -30,7 +31,7 @@ export const setAuthUserData = (userId, email, login, isAuth) => ({
 })
 
 export const getAuthUserData = () => (dispatch) => {
-    authAPI.me()
+    return authAPI.me()
         .then(response => {
             if (response.data.resultCode === 0) {
                 let {id, email, login} = response.data.data;
@@ -45,7 +46,11 @@ export const login = (email, password, rememberMe) => (dispatch) => {
         .then(response => {
             if (response.data.resultCode === 0) {
                 dispatch(getAuthUserData());
+            } else {
+               let message =  response.data.messages.length > 0 ? response.data.messages[0]: 'Some error';
+                dispatch(stopSubmit('login', {_error: 'Some Error'}));
             }
+
         });
 }
 
