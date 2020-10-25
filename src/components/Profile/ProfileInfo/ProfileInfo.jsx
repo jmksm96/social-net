dd998b3ba1cf680059467fcc6d1c8d5e085a5432
jmksm96 <60604825+jmksm96@users.noreply.github.com>
2020-10-25 17:32:53 +0300

    84: Hooks - useState

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css'
 import Preloader from "../../common/Preloader/Preloader";
-import ProfileStatus from './ProfileStatus'
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -17,7 +17,7 @@ const ProfileInfo = (props) => {
                 <div>{props.profile.contacts.vk}</div>
                 <div>{props.profile.contacts.instagram}</div>
                 <div>{props.profile.lookingForAJobDescription}</div>
-                <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+                <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
             </div>
         </div>
     )
