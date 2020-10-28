e21db8a754a9be6bfde150d6cc98e23e4c539089
Max <jmksm96@gmail.com>
2020-10-28 19:24:45 +0300

    Updates

---
+++ precache-manifest.f218205b1f2ebcfbab7ebbb930bfea6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d21d66f5ae277210d3eb96e12bbc3e6",
+    "url": "/social-net/index.html"
+  },
+  {
+    "revision": "20d9c4e9eb28c1ca36a3",
+    "url": "/social-net/static/css/main.93b497f4.chunk.css"
+  },
+  {
+    "revision": "744f3227c4fa3e82cd6a",
+    "url": "/social-net/static/js/2.da95aa4e.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/social-net/static/js/2.da95aa4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20d9c4e9eb28c1ca36a3",
+    "url": "/social-net/static/js/main.141f2090.chunk.js"
+  },
+  {
+    "revision": "b2fd0dd27886ef0390c2",
+    "url": "/social-net/static/js/runtime-main.71d8e7d5.js"
+  },
+  {
+    "revision": "6e7c9db7e1f100edac7c6e48714d2ea0",
+    "url": "/social-net/static/media/preloader.6e7c9db7.svg"
+  }
+]);
